ab7d9bd1e03216442402692d6dc49ea2dd37f824
Alex Hioreanu <git@hioreanu.net>
2010-09-01 17:15:51 +0000

    Updates from axh-x200.

--- emacs.d/custom-22.el
+++ emacs.d/custom-22.el
@@ -3,12 +3,15 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(js2-bounce-indent-p nil)
+ '(js2-indent-on-enter-key t)
+ '(load-home-init-file t t)
+ '(toolbar-visible-p nil))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(show-paren-match ((((class color) (background dark)) (:weight bold))))
- '(show-paren-match-face ((((class color)) (:background "black" :foreground "white")) (t (:background "gray"))) t)
- '(show-paren-mismatch-face ((((class color)) (:background "black" :foreground "red")) (t (:background "gray"))) t))
+ '(show-paren-mismatch ((((class color)) (:background "black" :foreground "red")) (t (:background "gray"))))
+ '(show-paren-mismatch-face ((((class color)) (:background "black" :foreground "red")) (t (:background "gray")))))
