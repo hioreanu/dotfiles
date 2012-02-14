24a1e824aacd639f68a8ea26257b4c808f1d5ce1
Alex Hioreanu <git@hioreanu.net>
2012-02-14 20:22:41 +0000

    Verify writing to a fifo, not a file that was accidentally created in place of the fifo.
    Open fifo once for writing.
    
    Die on write errors.

--- bin/mcabber-away.pl
+++ bin/mcabber-away.pl
@@ -2,19 +2,23 @@
 # $Id$
 # Monitor gnome-screensaver status and control Mcabber status accordingly.
 
+use IO::Handle;
+
 my $cmd = "dbus-monitor --session \"type='signal',interface='org.gnome.ScreenSaver',member='ActiveChanged'\"";
+my $fifo = "${HOME}/mcabber.fifo";
 
 local *IN, *OUT;
-open(IN, "$cmd |");
-# TODO: should open verify is fifo, open fifo once, error out on write errors.
-# open(OUT, ">>${HOME}/mcabber.fifo");
+open(IN, "$cmd |") or die "$cmd: $!";
+open(OUT, ">${fifo}") or die "open fifo ${fifo} for writing: $!";
+OUT->autoflush(1);
+die if not -p OUT;
 
 while (<IN>) {
   if (m/^\s+boolean true/) {
-    system "echo /status away > ${HOME}/mcabber.fifo";
+    print OUT "/status away\n" or die "Write to fifo ${fifo}: $!";
     print "/status away\n";
   } elsif (m/^\s+boolean false/) {
-    system "echo /status online > ${HOME}/mcabber.fifo";
+    print OUT "/status online\n" or die "Write to fifo ${fifo}: $!";
     print "/status online\n";
   }
 }
